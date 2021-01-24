732cf506fff259d3dd08581d1a10962a47fd767b
Your Name <you@example.com>
2021-01-24 07:49:27 +0400

    added third page to url

--- main/views.py
+++ main/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render, HttpResponse
 
 def homepage(request):
     return HttpResponse('Hello world')
+def third(request):
+    return HttpResponse('This is page test3')
