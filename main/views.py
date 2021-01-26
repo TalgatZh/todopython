2d411b4cb779f4bfe2d154f969eebe5417c7fb24
Your Name <you@example.com>
2021-01-26 09:50:39 +0400

    added todo unmarking

--- main/views.py
+++ main/views.py
@@ -25,6 +25,6 @@ def delete_todo(request, id):
 
 def mark_todo(request, id):
     todo=ToDo.objects.get(id=id)
-    todo.is_favorite=True
+    todo.is_favorite=not todo.is_favorite
     todo.save()
     return redirect(test)
